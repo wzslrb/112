52fdace40375e0adb63c8f379b36443f4e958583
wzslrb <67636812+wzslrb@users.noreply.github.com>
2020-09-07 16:06:02 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#git clone https://github.com/fw876/helloworld.git                                   package/molun/luci-app-ssr-plus
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/molun/luci-app-adguardhome
+#git clone https://github.com/vernesong/OpenClash.git                                package/molun/luci-app-openclash
+pwd
+echo "初始化……"
+git clone https://github.com/wzslrb/xf_b70.git 
+echo "初始化……"
+git clone https://github.com/hyird/Action-Openwrt.git
+ls -al
