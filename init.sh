b00698188f3670295add5cb1a85594903b61854e
wzslrb <67636812+wzslrb@users.noreply.github.com>
2020-09-07 17:06:30 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -21,5 +21,24 @@ ls -al
 echo "查看补丁目录"
 ls -al ./user/lean-mt7621/files/
 
-
+cd ./user/lean-mt7621/
+# 在这里指定你的OpenWrt的Repo URL
+#REPO_URL="https://github.com/coolsnowwolf/lede"
+# 在这里指定你的OpenWrt的Branch
+#REPO_BRANCH="master"
+#上传 Packages 目录
+sed -i  's/UPLOAD_PACKAGES_DIR.*/UPLOAD_PACKAGES_DIR="false"/g' settings.ini
+#上传 Targets 目录
+sed -i  's/UPLOAD_TARGETS_DIR.*/UPLOAD_TARGETS_DIR="false"/g' settings.ini
+#上传固件
+sed -i  's/UPLOAD_FIRMWARE.*/UPLOAD_FIRMWARE="true"/g' settings.ini
+# 上传到 Artifacts
+sed -i  's/UPLOAD_TO_ARTIFACTS.*/UPLOAD_TO_ARTIFACTS="true"/g' settings.ini
+# 发布 Realease
+sed -i  's/UPLOAD_TO_REALEASE.*/UPLOAD_TO_REALEASE="true"/g' settings.ini
+# 上传到 奶牛快传
+sed -i  's/UPLOAD_TO_COWTRANSFER.*/UPLOAD_TO_COWTRANSFER="true"/g' settings.ini
+# 微信通知
+sed -i  's/WECHAT_NOTIFICATION.*/WECHAT_NOTIFICATION="false"/g' settings.ini
+cat settings.ini
 exit 0
