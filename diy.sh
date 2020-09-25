8e07ff1829bcbb75363eae5be0030233dec7a7f7
烂橙饶命Ⅱ <67636812+wzslrb@users.noreply.github.com>
2020-09-25 19:49:17 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -70,6 +70,8 @@ grep -q "^[^#].*_rename=" .config || sed -i '$a CONFIG_PACKAGE_rename=y' .config
 sed -i '$a CONFIG_PACKAGE_diffutils=y' .config
 sed -i '$a CONFIG_PACKAGE_patch=y' .config
 sed -i '$a\CONFIG_PACKAGE_sshpass=y' .config
+#互动的进程查看器
+sed -i '$a\CONFIG_PACKAGE_htop=y' .config
 
 # 主题
 sed -i "/.*luci-theme-freifunk-generic.*=m/s/=m/=y/g" .config
