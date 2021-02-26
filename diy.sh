63f8eea3ace5dc9078cd22760c13cf34906998c5
wzslrb <yanriqingquan@gmail.com>
2021-02-26 11:58:59 +0000

    n

--- diy.sh
+++ diy.sh
@@ -22,14 +22,14 @@ sed -i -e '/CONFIG_TARGET_DEVICE/ { /5962/b' -e 'N; d; }' .config
 #<<'COMMENT'
 grep -q "^[^#].*apfree-wifidog.*=" .config || sed -i '$a CONFIG_PACKAGE_apfree-wifidog=y' .config && echo "add apfree-wifidog"
 #grep -q "^[^#].*luci-app-frpc.*=" .config || sed -i '$a CONFIG_PACKAGE_luci-app-frpc=y' .config
-grep -q "^[^#].*_tree.*=" .config || sed -i '$a CONFIG_PACKAGE_tree=y' .config
-grep -q "^[^#].*_git-http.*=" .config || sed -i '$a CONFIG_PACKAGE_git-http=y' .config
+# grep -q "^[^#].*_tree.*=" .config || sed -i '$a CONFIG_PACKAGE_tree=y' .config
+# grep -q "^[^#].*_git-http.*=" .config || sed -i '$a CONFIG_PACKAGE_git-http=y' .config
 grep -q "^[^#].*_curl.*=" .config || sed -i '$a CONFIG_PACKAGE_curl=y' .config
 grep -q "^[^#].*_wget-nossl.*=" .config || sed -i '$a CONFIG_PACKAGE_wget-nossl=y' .config
 grep -q "^[^#].*openssh-client.*=" .config || sed -i '$a CONFIG_PACKAGE_openssh-client=y' .config
 
 sed -i "/.*automount.*=m/s/=m/=y/g" .config
-sed -i "/.*autosamba.*=m/s/=m/=y/g" .config
+# sed -i "/.*autosamba.*=m/s/=m/=y/g" .config
 sed -i "/.*filetransfer.*=m/s/=m/=y/g" .config
 sed -i "/.*fileassistant.*=m/s/=m/=y/g" .config
 
@@ -45,7 +45,7 @@ sed -i "/.*iptables-mod-length2=m/s/=m/=y/g" .config
 ## SQM
 sed -i "/.*sqm.*=m/s/=m/=y/g" .config
 #sed -i "/.*frpc.*=m/s/=m/=y/g" .config
-sed -i "/.*aria2.*=m/s/=m/=y/g" .config
+# sed -i "/.*aria2.*=m/s/=m/=y/g" .config
 #sed -i "/.*ariang.*=m/s/=m/=y/g" .config
 #sed -i "/.*webui-aria2.*=m/s/=m/=y/g" .config
 #grep -q "^[^#].*webui-aria2.*=" .config || sed -i '$a CONFIG_PACKAGE_webui-aria2=y' .config
@@ -53,7 +53,7 @@ sed -i "/.*vsftpd.*=m/s/=m/=y/g" .config
 
 #带宽、流量监测
 sed -i "/.*nlbwmon.*=m/s/=m/=y/g" .config
-#sed -i "/.*wrtbwmon.*=m/s/=m/=y/g" .config
+sed -i "/.*wrtbwmon.*=m/s/=m/=y/g" .config
 #穿透
 sed -i "/.*zerotier=m/s/=m/=y/g" .config
 
@@ -78,6 +78,8 @@ sed -i "/.*ddns.*=m/s/=m/=y/g" .config
 # 科学上网
 sed -i "/.*openclash.*=m/s/=m/=y/g" .config
 sed -i "/.*ssr-plus.*=m/s/=m/=y/g" .config
+sed -i "/.*app-vssr=m/s/=m/=y/g" .config
+
 #luci-app-ssr-plus
 #sed -i "/.*luci-app-ssr-plus.*=m/s/=m/=y/g" .config
 #sed -i "/luci-app-ssr-plus=y/a CONFIG_PACKAGE_luci-i18n-ssr-plus-zh-cn=y" .config
@@ -85,31 +87,34 @@ sed -i "/.*ssr-plus.*=m/s/=m/=y/g" .config
 sed -i "/.*curl.*=m/s/=m/=y/g" .config
 sed -i "/.*wget.*=m/s/=m/=y/g" .config
 sed -i "/.*wget-nossl.*=m/s/=m/=y/g" .config
-sed -i "/.*tree.*=m/s/=m/=y/g" .config
-sed -i "/.*lscpu.*=m/s/=m/=y/g" .config
-sed -i "/.*lsof.*=m/s/=m/=y/g" .config
-sed -i "/.*lrzsz.*=m/s/=m/=y/g" .config
-sed -i "/.*bash.*=m/s/=m/=y/g" .config
-grep -q "^[^#].*_rename=" .config || sed -i '$a CONFIG_PACKAGE_rename=y' .config
-sed -i '$a CONFIG_PACKAGE_diffutils=y' .config
-sed -i '$a CONFIG_PACKAGE_patch=y' .config
+# lsofsed -i "/.*tree.*=m/s/=m/=y/g" .config
+# lsofsed -i "/.*lscpu.*=m/s/=m/=y/g" .config
+# sed -i "/.*lsof.*=m/s/=m/=y/g" .config
+# sed -i "/.*lrzsz.*=m/s/=m/=y/g" .config
+# sed -i "/.*bash.*=m/s/=m/=y/g" .config
+# grep -q "^[^#].*_rename=" .config || sed -i '$a CONFIG_PACKAGE_rename=y' .config
+# sed -i '$a CONFIG_PACKAGE_diffutils=y' .config
+# sed -i '$a CONFIG_PACKAGE_patch=y' .config
 sed -i '$a\CONFIG_PACKAGE_sshpass=y' .config	#SSH免密码
-sed -i '$a\CONFIG_PACKAGE_netperf=y' .config	#网络测速
-sed -i '$a\CONFIG_PACKAGE_speedtest-netperf=y' .config	#网络测速脚本
-sed -i '$a\CONFIG_PACKAGE_speedtest-cli=y' .config	#C语言测速
-sed -i '$a\CONFIG_PACKAGE_coreutils=y' .config
-sed -i '$a\CONFIG_PACKAGE_coreutils-nohup=y' .config	#nohup后台允许
-sed -i '$a\CONFIG_PACKAGE_screen=y' .config
+# sed -i '$a\CONFIG_PACKAGE_netperf=y' .config	#网络测速
+# sed -i '$a\CONFIG_PACKAGE_speedtest-netperf=y' .config	#网络测速脚本
+# sed -i '$a\CONFIG_PACKAGE_speedtest-cli=y' .config	#C语言测速
+# sed -i '$a\CONFIG_PACKAGE_coreutils=y' .config
+# sed -i '$a\CONFIG_PACKAGE_coreutils-nohup=y' .config	#nohup后台允许
+# sed -i '$a\CONFIG_PACKAGE_screen=y' .config
 
 #互动的进程查看器
-sed -i '$a\CONFIG_PACKAGE_htop=y' .config
+# sed -i '$a\CONFIG_PACKAGE_htop=y' .config
 #iw
 sed -i '$a\CONFIG_PACKAGE_iw=y' .config
 sed -i '$a\CONFIG_PACKAGE_iwinfo=y' .config
+sed -i '$a\CONFIG_KERNEL_KEXEC=y' .config
+
 # 主题
 sed -i "/.*luci-theme-opentomato.*=m/s/=m/=y/g" .config
 sed -i "/.*luci-theme-atmaterial.*=m/s/=m/=y/g" .config
 sed -i "/.*luci-theme-netgear.*=m/s/=m/=y/g" .config
+sed -i "/.*luci-theme-argon=m/s/=m/=y/g" .config
 
 #新近更新=y 去除https-dns-proxy默认安装
 sed -i "/^CONFIG.*dns-proxy.*/{s/^/# &/g; s/=.*/ is not set/g}" .config && echo "去除https-dns-proxy默认安装"
